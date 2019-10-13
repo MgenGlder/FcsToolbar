d59a8bf64a9b91a0863899d32333bca0a9d469ca
Kunle Oshiyoye <mgen.glder@gmail.com>
2019-10-13 19:29:26 -0400

    Fix things

--- src/renderer/store/modules/UserConfiguration.js
+++ src/renderer/store/modules/UserConfiguration.js
@@ -14,16 +14,17 @@ const state = {
 }
 const mutations = {
   SET_FOLDER_LOCATIONS (state, payload) {
-    if (payload === undefined) {
+    console.log('modifying current location to... ', payload)
+    if (payload === undefined)
       return
-    }
     // Store runs on the back end, so we have to use the IPCMain to to interact directly with the backend;
     // Also, because the same store code runs on the back end main process as well as the renderer, we have to use flags 
     // to guarantee that this code is only run on the backend store
-    mockVue.$electron.ipcMain._events.updateFolderConfigurations({}, state.folderLocations)
+
     state.folderLocations.fcs = payload.fcs
     state.folderLocations.fcsWeb = payload.fcsWeb
     state.folderLocations.fcsUi = payload.fcsUi
+    mockVue.$electron.ipcMain._events.updateFolderConfigurations({}, state.folderLocations)
   }
 }
 
